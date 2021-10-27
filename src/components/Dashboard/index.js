24cc23316f5831ee4623bf8267a4735c4144f5dd
Gabriel Wolf <65361452+GabrielWolf-Dev@users.noreply.github.com>
2021-10-26 21:03:01 -0300

    Input de pesquisa

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { auth } from '../../firebase';
 import { useAuth } from '../../hooks/useAuth';
+import Search from '@material-ui/icons/Search';
+import ViewList from '@material-ui/icons/ViewList';
 
 import Header from '../Header';
 import Footer from '../Footer';
@@ -10,12 +12,18 @@ import Logout from '../../assets/svg/logout.svg';
 
 import {
     TitleBigger,
+    Input,
 } from '../UI';
 import {
     NavAccount,
     ImgAccount,
     FigCaptionImg,
     ButtonIcon,
+    AccountContainerFlex,
+    BoxInputSearch,
+    BoxFilter,
+    SelectFilter,
+    LabelSearch
 } from './style';
 
 export default function Dashboard() {
@@ -52,6 +60,34 @@ export default function Dashboard() {
                     </ButtonIcon>
                 </div>
             </NavAccount>
+
+            <AccountContainerFlex>
+                <BoxInputSearch>
+                    <Input
+                        id="search"
+                        type="text"
+                        placeholder="Pesquisar"
+                    />
+
+                    <LabelSearch htmlFor="search">
+                        <Search />
+                    </LabelSearch>
+                </BoxInputSearch>
+
+                <BoxFilter>
+                    <SelectFilter name="filter_files">
+                        <option value="all">Todos</option>
+                        <option value="phtos">Fotos</option>
+                        <option value="documents">Documentos</option>
+                    </SelectFilter>
+
+                    
+                    <ViewList
+                        style={{ cursor: 'pointer', fontSize: '32px' }}
+                        onClick={() => alert('Trocar para o sistema de coluna')}
+                    />
+                </BoxFilter>
+            </AccountContainerFlex>
             <Footer />
         </>
     )
--- src/components/Dashboard/style.js
+++ src/components/Dashboard/style.js
@@ -18,6 +18,53 @@ export const NavAccount = styled.nav`
     }
 `;
 
+export const AccountContainerFlex = styled.section`
+    width: 100%;
+    max-width: 300px;
+    margin: 32px auto;
+    padding: 0 2%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 32px;
+
+    @media (min-width: 768px) {
+        flex-direction: row;
+        justify-content: space-between;
+    }
+
+    @media (min-width: 368px) { max-width: 400px; }
+    @media (min-width: 768px) { max-width: 1280px; }
+`;
+
+export const BoxInputSearch = styled.div`
+    width: 100%;
+    max-width: 300px;
+    padding: 0 2%;
+    height: 42px;
+    position: relative;
+`;
+
+export const BoxFilter = styled.div`
+    width: 100%;
+    max-width: 300px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid ${black};
+`;
+
+export const LabelSearch = styled.label`
+    position: absolute;
+    top: 60%;
+    right: 16px;
+    transform: translateY(-60%);
+    color: ${black};
+
+    @media (min-width: 768px) { right: 36px; }
+`;
+
 export const ImgAccount = styled.img`
     width: 42px;
     height: 42px;
@@ -56,4 +103,11 @@ export const ButtonIcon = styled.button`
         width: 42px;
         height: 42px;
     }
+`;
+
+export const SelectFilter = styled.select`
+    border: 0;
+    font-size: 1rem;
+    padding: 8px 0;
+    cursor: pointer;
 `;
