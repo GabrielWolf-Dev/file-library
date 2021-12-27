1ed92682279f2339f36a868312fbe4977dd7f18e
GabrielWolf-Dev <gabrielw.contato@gmail.com>
2021-12-27 17:24:55 -0300

    ALteraçao no icone e finalizaçao da pagina 'Creditos'

--- src/components/Credits/style.js
+++ src/components/Credits/style.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { Container, Content } from '../UI';
+import { cianBlue, orange } from '../UI/colors';
 
 export const ContainerCredits = styled(Container)`
     margin: 48px auto;
@@ -34,5 +35,41 @@ export const Img = styled.img`
 
     @media (min-width: 768px) {
         width: 100%;
+    }
+`;
+
+export const ListContributions = styled.ul`
+    width: 100%;
+    max-width: 300px;
+    height: 100%;
+    margin: 48px auto;
+    padding: 0 2%;
+    list-style-type: none;
+    text-align: center;
+    font-size: 1.15rem;
+
+    @media (min-width: 468px) { max-width: 440px; }
+
+    @media (min-width: 568px) { max-width: 540px; }
+
+    @media (min-width: 768px) {
+        max-width: 1280px;
+        margin: 100px auto 48px auto;
+    }
+`;
+
+export const ListItem = styled.li`
+    margin: 8px 0;
+`;
+
+export const LinkItems = styled.a`
+    color: ${cianBlue};
+    font-weight: 500;
+    text-decoration: underline;
+    transition: all ease-in .3s;
+
+    &:hover {
+        opacity: 0.8;
+        color: ${orange};
     }
 `;
